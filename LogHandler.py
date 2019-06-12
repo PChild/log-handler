f91e2f11f147a46e5a51a9737b8360763fca0eb6
Preston <childrep@vt.edu>
2019-06-12 12:55:19 -0400

    Add log archiving, export functional, fixed bug with blank lines.

--- LogHandler.py
+++ LogHandler.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from zipfile import ZipFile
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtGui
@@ -19,7 +21,10 @@ class LogHandler(QWidget):
         self.log_dir = self.config['DEFAULT']['LogLocation']
         self.output_dir = self.config['DEFAULT']['OutputLocation']
 
-        self.filter = "Matches"
+        self.filter = "Match"
+
+        self.log_data = None
+        self.update_files_data()
 
         self.title = 'DS Log Handler'
         self.setWindowIcon(QtGui.QIcon('icon.png'))
@@ -41,8 +46,9 @@ class LogHandler(QWidget):
         self.setFixedSize(self.width, self.height)
 
         self.log_row = ButtonRow("Log Folder:", self.log_dir)
+        self.connect_to_logs(self.log_row)
         self.out_row = ButtonRow("Out Folder:", self.output_dir)
-        self.type_radios = RadioGroup("Log Type:", "Matches", "Practice", "Both")
+        self.type_radios = RadioGroup("Log Type:", "Match", "Practice", "Both")
         self.connect_to_radios(self.type_radios)
 
         self.list_view = QListView()
@@ -50,11 +56,22 @@ class LogHandler(QWidget):
         self.list_model = QtGui.QStandardItemModel(self.list_view)
         self.update_list_view()
 
-        click_check = QPushButton('Export selected logs')
-        click_check.clicked.connect(self.convert_files)
+        export_btn = QPushButton('Export selected logs')
+        export_btn.setToolTip('Creates CSV copies of selected logs in the output folder.')
+        export_btn.setStyleSheet("color: #28a745")
+        export_btn.clicked.connect(self.convert_files)
+
+        archive_btn = QPushButton('Archive selected logs')
+        archive_btn.setToolTip('Moves selected logs into a zip archive.')
+        archive_btn.setStyleSheet("color: #dc3545")
+        archive_btn.clicked.connect(self.archive_files)
+
+        btn_line = QHBoxLayout()
+        btn_line.addWidget(export_btn)
+        btn_line.addWidget(archive_btn)
 
         self.status_line = QLineEdit()
-        self.status_line.setText("")
+        self.status_line.setText("Ready.")
         self.status_line.setEnabled(False)
 
         layout = QVBoxLayout()
@@ -62,38 +79,52 @@ class LogHandler(QWidget):
         layout.addLayout(self.out_row)
         layout.addLayout(self.type_radios)
         layout.addWidget(self.list_view)
-        layout.addWidget(click_check)
+        layout.addLayout(btn_line)
         layout.addWidget(self.status_line)
         self.setLayout(layout)
         self.show()
 
     def update_list_view(self):
-        log_files = [item[len(self.log_dir):] for item in glob.glob(self.log_dir + '*.dslog')]
         self.list_model.removeRows(0, self.list_model.rowCount())
-        for log in log_files:
-            if self.filter == "Matches":
-                if os.path.exists(self.log_dir + log[:-6] + '.dsevents'):
-                    self.list_model.appendRow(LogListItem(log))
+        for log in self.log_data:
+            if self.filter == "Match":
+                if log['is_match']:
+                    self.list_model.appendRow(LogListItem(log['name']))
             elif self.filter == "Practice":
-                if not os.path.exists(self.log_dir + log[:-6] + '.dsevents'):
-                    self.list_model.appendRow(LogListItem(log))
+                if not log['is_match']:
+                    self.list_model.appendRow(LogListItem(log['name']))
             elif self.filter == "Both":
-                self.list_model.appendRow(LogListItem(log))
-        self.list_view.setModel(self.list_model)
+                self.list_model.appendRow(LogListItem(log['name']))
+            self.list_view.setModel(self.list_model)
 
-    def convert_files(self):
-        self.prep_out_location()
+    def update_files_data(self):
+        log_data = []
+        log_files = [item[len(self.log_dir):] for item in glob.glob(self.log_dir + '*.dslog')]
 
+        for log in log_files:
+            try:
+                match_info = dslog2csv.find_match_info(self.log_dir + log[:-6] + '.dsevents')
+                log_data.append({'name': log, 'is_match': match_info is not None})
+            except:
+                pass
+
+        self.log_data = log_data
+
+    def get_selected_files(self):
         log_files = []
         for index in range(self.list_model.rowCount()):
             item = self.list_model.item(index)
             if item.checkState() == 2:
-                file_type = item.text().split(".")[1]
-                if file_type == "dslog":
-                    log_files.append(item.text())
+                log_files.append(item.text())
+        return log_files
 
-        # CODE BELOW IS REIMPLEMENTED FROM DSLOG2CSV
-        get_match_info = self.filter == "Matches"
+    def convert_files(self):
+        self.prep_out_location()
+
+        log_files = self.get_selected_files()
+
+        # CODE BELOW IS REIMPLEMENTED FROM DSLOG2CSV'S MAIN
+        get_match_info = self.filter == "Match"
 
         col = ['inputfile', ]
         if get_match_info:
@@ -105,17 +136,17 @@ class LogHandler(QWidget):
             in_file = self.log_dir + in_name
             match_info = None
 
-            if get_match_info:
-                event_file = dslog2csv.find_event_file(in_file)
-                event_parser = dslog2csv.DSEventParser(event_file)
+            # this block might crash, check it
+            event_file = dslog2csv.find_event_file(in_file)
+            event_parser = dslog2csv.DSEventParser(event_file)
 
-                if event_file and event_parser.version == 3:
-                    try:
-                        match_info = dslog2csv.find_match_info(event_file)
-                    except:
-                        pass
-                else:
-                    self.status_line.setText(in_name + " had bad version, skipping.")
+            if event_file and event_parser.version == 3:
+                try:
+                    match_info = dslog2csv.find_match_info(event_file)
+                except:
+                    self.status_line.setText(in_name + " had bad match info, skipping.")
+                    problem_files += 1
+                    pass
 
             if get_match_info and not match_info:
                 self.status_line.setText(in_name + " had bad match info, skipping.")
@@ -123,7 +154,7 @@ class LogHandler(QWidget):
                 continue
 
             outname = self.output_dir + in_name[:-6] + ".csv"
-            outstrm = open(outname, 'w')
+            outstrm = open(outname, 'w', newline='')
             outcsv = csv.DictWriter(outstrm, fieldnames=col, extrasaction='ignore')
             outcsv.writeheader()
 
@@ -133,10 +164,12 @@ class LogHandler(QWidget):
                 self.status_line.setText(in_file + " had bad version, skipping.")
                 problem_files += 1
                 continue
+                
             if dsparser.version != 3:
                 self.status_line.setText(in_file + " had bad version, skipping.")
                 problem_files += 1
                 continue
+                
             for rec in dsparser.read_records():
                 rec['inputfile'] = in_file
                 rec['match_info'] = match_info
@@ -147,8 +180,26 @@ class LogHandler(QWidget):
                 outcsv.writerow(rec)
             dsparser.close()
             outstrm.close()
-        self.status_line.setText("Processing complete. Skipped " + str(problem_files) + "/"
-                                 + str(len(log_files)) + " log files.")
+        file_cnt = len(log_files)
+        file_str = str(file_cnt)
+        success = str(file_cnt - problem_files) + "/" + file_str
+        fails = str(problem_files) + "/" + file_str
+        self.status_line.setText("Export complete. Processed  " + success + " log files, skipped " + fails + " files.")
+
+    def archive_files(self):
+        self.status_line.setText("Archiving files...")
+        log_files = self.get_selected_files()
+
+        time_str = datetime.now().strftime('%Y_%m_%d %H_%M_%S')
+        file_name = self.log_dir + time_str + " ARCHIVE - " + str(len(log_files)) + " LOG FILES.zip"
+
+        with ZipFile(file_name, 'w') as z_file:
+            for file in log_files:
+                z_file.write(self.log_dir + file)
+                os.remove(self.log_dir + file)
+        self.status_line.setText("Archived " + str(len(log_files)) + " log files.")
+        self.update_files_data()
+        self.update_list_view()
 
     def check_config(self):
         config = configparser.ConfigParser()
@@ -184,6 +235,18 @@ class LogHandler(QWidget):
     def connect_to_radios(self, radio_obj):
         radio_obj.changed.connect(self.on_changed)
 
+    @pyqtSlot(str)
+    def on_folder_changed(self, string):
+        self.log_dir = string
+        self.config.set('DEFAULT', 'LogLocation', self.log_dir)
+        print(self.log_dir)
+        self.write_config(self.config)
+        self.update_files_data()
+        self.update_list_view()
+
+    def connect_to_logs(self, btn_obj):
+        btn_obj.folder_change.connect(self.on_folder_changed)
+
 
 class LogListItem(QtGui.QStandardItem):
     def __init__(self, title):
@@ -192,6 +255,8 @@ class LogListItem(QtGui.QStandardItem):
 
 
 class ButtonRow(QHBoxLayout):
+    folder_change = pyqtSignal(str)
+
     def __init__(self, label, text):
         super().__init__()
 
@@ -207,9 +272,10 @@ class ButtonRow(QHBoxLayout):
         self.addWidget(self.folder_picker)
 
     def pick_folder(self):
-        folder_name = QFileDialog.getExistingDirectory(directory=self.line_edit.text())
-        if len(folder_name) > 0:
+        folder_name = QFileDialog.getExistingDirectory(directory=self.line_edit.text()) + "/"
+        if len(folder_name) > 1:
             self.line_edit.setText(folder_name)
+            self.folder_change.emit(folder_name)
 
 
 class RadioGroup(QHBoxLayout):
